5d9b06604517ccda13196edfea7fb034ea869659
Nate Nystrom <nate.nystrom@usi.ch>
2014-12-14 22:38:09 +0100

    first attempt at webpage for L&C 2015

---
+++ schedule.rb
@@ -0,0 +1,96 @@
+load 'scheduler.rb'
+
+s = schedule do
+  day   'Mo'
+  day   'We'
+
+  first '2015.02.16'
+  last  '2015.05.29'
+
+  except '2014.03.19', '<i>No class: San Giuseppe</i>'
+  except '2014.04.03', '<i>No class: Pasqua</i>'
+  except '2014.04.04', '<i>No class: Pasqua</i>'
+  except '2014.04.05', '<i>No class: Pasqua</i>'
+  except '2014.04.06', '<i>No class: Pasqua</i>'
+  except '2014.04.07', '<i>No class: Pasqua</i>'
+  except '2014.04.08', '<i>No class: Pasqua</i>'
+  except '2014.04.09', '<i>No class: Pasqua</i>'
+  except '2014.04.10', '<i>No class: Pasqua</i>'
+  except '2014.04.11', '<i>No class: Pasqua</i>'
+  except '2014.04.12', '<i>No class: Pasqua</i>'
+  except '2014.05.01', '<i>No class: Festa dei lavoratori</i>'
+  except '2014.05.14', '<i>No class: Ascensione</i>'
+  except '2014.05.25', '<i>No class: Pentecoste</i>'
+
+  section "Introduction" do
+  end
+
+  lecture "Introduction" do
+  end
+
+  lecture "Haskell crash course" do
+    prep '<a href="http://learnyouahaskell.com/chapters">Learn You a Haskell</a> Ch. 1&ndash;3'
+  end
+
+  lecture "Recursion, datatypes, and pattern matching" do
+    prep '<a href="http://learnyouahaskell.com/chapters">Learn You a Haskell</a> Ch. 4&ndash;6, 8'
+  end
+
+  lecture "Types, higher-order functions" do
+  end
+
+  lecture "Advanced functional programming" do
+  end
+
+  lecture "Functors, Applicatives, Monads" do
+  end
+
+  lecture "Bind, State Monad, Introduction to Parsec" do
+  end
+
+  section "Interpreters" do
+  end
+
+  lecture "Interpreters" do
+  end
+
+  lecture "Operational semantics" do
+  end
+
+  lecture "Functions, scope, control flow" do
+  end
+
+  lecture "Static typing and typing rules" do
+  end
+
+  lecture "Static semantics" do
+  end
+
+  exam do
+     date '2015.04.20'
+     title 'Midterm'
+  end
+
+  section "Compilers" do
+  end
+
+  lecture "Recursive-descent Parsing" do
+  end
+
+  lecture "Parser combinators" do
+  end
+
+  lecture "Closure conversion" do
+  end
+
+  lecture "Intermediate representations" do
+  end
+
+  lecture "Code generation" do
+  end
+
+  lecture "Register allocation" do
+  end
+end
+
+puts s.to_html
